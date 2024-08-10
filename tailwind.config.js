4581a50cfc8f9dc9a9127b1203badb4253df65c3
Liam <Badusername64152@gmail.com>
2024-08-10 15:11:49 -0700

    nav bar created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        screens: {
+          xs: '480px',   // Extra small screens (e.g., small mobile devices)
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
+          '3xl': '1920px',  // 3x extra-large screens (e.g., 1080p monitors)
+          '4xl': '2560px',  // 4x extra-large screens (e.g., 1440p monitors)
+          '5xl': '3200px',  // 5x extra-large screens (e.g., ultra-wide monitors)
+        },
+      },
+    },
   },
   plugins: [],
 }
